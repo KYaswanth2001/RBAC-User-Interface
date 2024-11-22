9a0bcd919135a2e4750bc07cfc1346d345b4d18c
KYaswanth2001 <kotiyaswanth2001@gmail.com>
2024-11-22 11:42:53 +0530

    Create PermissionMatrix.js

---
+++ redux/components/PermissionMatrix.js
@@ -0,0 +1,6 @@
+const PermissionMatrix = () => {
+  // Dummy component to link roles and permissions
+  return <div>Permission Matrix UI Coming Soon...</div>;
+};
+
+export default PermissionMatrix;
